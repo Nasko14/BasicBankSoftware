323b2a367e38ad482af57a2fb9522515ff42baf2
Atanas Atanasov <nasko200406@gmail.com>
2022-02-09 22:51:52 +0200

    ADDED some test methods in User
    Testing the logic of the app

--- src/Management.java
+++ src/Management.java
@@ -1,2 +1,6 @@
+import java.util.Vector;
+
 public class Management {
+    Vector<User> users;
+
 }
--- src/User.java
+++ src/User.java
@@ -1,2 +1,62 @@
+import java.util.Scanner;
+import java.util.Vector;
+
 public class User {
+    String userType;
+    String username;
+    String password;
+    Vector<Bill> bills;
+    Vector<Credit> credits;
+    double creditScore;
+
+    public void setUserType(String userType) {
+        switch (userType) {
+            case "Banker":
+                this.userType = "Banker";
+                break;
+            case "User":
+                this.userType = "User";
+                break;
+            default:
+                do {
+                    System.out.println("Not a valid user type");
+                    Scanner scanner = new Scanner(System.in);
+
+                    System.out.println("Enter the type here: ");
+                    userType = scanner.nextLine();
+                } while (!userType.equals("User") && !userType.equals("Banker"));
+
+                break;
+        }
+    }
+
+    public String getUserType() {
+        return this.userType;
+    }
+
+    public void setUsername(String username, Management management) {
+        for (int index = 0; index < management.users.size(); index++) {
+            if (username.equals(management.users.get(index).getUsername())) {
+                do {
+                    System.out.println("The username already exists");
+                    Scanner scanner = new Scanner(System.in);
+
+                    System.out.println("Enter another username here:");
+                    username = scanner.nextLine();
+                } while (username.equals(management.users.get(index).getUsername()));
+            }
+            this.username = username;
+        }
+    }
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setPassword() {
+
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
 }
